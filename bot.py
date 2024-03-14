90f3250987f5f74e63301e355cea565d3a09f394
Shreyan Paliwal <shreyan.paliwal@gmail.com>
2024-03-14 09:09:01 -0700

    Commit for 2024-03-14

--- bot.py
+++ bot.py
@@ -724,7 +724,7 @@ async def reactstats(ctx, post_id, role_needed = None):
     @returns: None
     '''
     post_id = helper.parse_type(int, post_id)
-    role_needed = helper.parse_type(int, role_needed) if role_needed != None else constants["year_role"]
+    role_needed = helper.parse_type(int, role_needed). if role_needed is not None else constants["year_role"]
     print(role_needed)
     
     try:
